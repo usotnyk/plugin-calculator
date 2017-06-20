5067a72016f908eaaded0c52e9e97e4fbaffc3b5
Julia Sotnyk <ulia_sotnik@hotmail.com>
2017-06-19 20:23:09 -0400

    refactored code to split up into separate functions

--- plugins/Loan Calculator/jsCalculations/js/main_calculator.js
+++ plugins/Loan Calculator/jsCalculations/js/main_calculator.js
@@ -9,12 +9,10 @@ var Loan = function Loan(amount, term) {
 var LoanCalculator = {
 
   calculatePayments: function (loan, interest) {
-    //console.log(loan);
     var discountFactor = this.getDiscountFactor(interest, loan.term)
     //can't divide by 0
     if (discountFactor != 0) {
       var result = loan.amount / discountFactor;
-      //console.log(result);
       return result;
     } else {
       console.log("discount factor can't equal to 0!");
@@ -23,22 +21,15 @@ var LoanCalculator = {
   },
   // private function
   getDiscountFactor: function (interest, term) {
-    console.log("calculating discountFactor");
     var i = interest/26;
-    //console.log(i);
     var n = (26*term)/12;
-    //console.log(n);
     var numerator = Math.pow((1 + i), n) - 1;
-    //console.log(numerator);
     var demoninator = i * Math.pow((1 + i), n);
-    //console.log(demoninator);
     var discountFactor = numerator/demoninator;
-    //console.log(discountFactor);
 
     return discountFactor;
   }
 }
-
 
 // client application - new instance of loan and calculations with diffrent interests
 
@@ -46,30 +37,43 @@ var LoanCalculator = {
 //minimumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.12)
 //maximumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.42);
 
-function onInputChange() {
-  console.log("slider-amount mouseup");
+function recalculate() {
   var amountValue = $("#slider-amount").slider("value");
   var termValue = $("#slider-length").slider("value");
-  console.log(amountValue);
-  console.log(termValue);
-  loan = new Loan(amountValue, termValue);
-  minimumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.0899)
-  maximumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.2999);
-  console.log("minimum payment amount is " + minimumPaymentAmount);
-  console.log("maximum payment amount is " + maximumPaymentAmount);
+
+  var paymentInformation = getPaymentInformation(amountValue, termValue);
+  paymentInformation.inspect();
+  displayPaymentInformation(paymentInformation);
 }
 
-$("#slider-amount").mouseup(onInputChange);
-  // console.log("slider-amount mouseup");
-  // var amountValue = $("#slider-amount").slider("value");
-  // var termValue = $("#slider-length").slider("value");
-  // console.log(amountValue);
-  // console.log(termValue);
-  // loan = new Loan(amountValue, termValue);
-  // minimumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.0899)
-  // maximumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.2999);
-  // console.log("minimum payment amount is " + minimumPaymentAmount);
-  // console.log("maximum payment amount is " + maximumPaymentAmount);
-// });
+function getPaymentInformation(amountValue, termValue) {
+  var paymentInformation = {};
 
-$("#slider-length").mouseup(onInputChange);
+  var loan = new Loan(amountValue, termValue);
+  paymentInformation.minimumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.0899)
+  paymentInformation.maximumPaymentAmount = LoanCalculator.calculatePayments(loan, 0.2999);
+  paymentInformation.inspect = function() {
+    console.log(this.minimumPaymentAmount);
+    console.log(this.maximumPaymentAmount);
+
+  }
+  //add other calculations as properties
+
+  return paymentInformation;
+}
+
+function displayPaymentInformation(paymentInformation) {
+ 
+  var minimumPaymentContainer = document.getElementById("minimum-payment");
+  minimumPaymentContainer.innerHTML = Math.round(paymentInformation.minimumPaymentAmount);
+  var maximumaymentContainer = document.getElementById("maximum-payment");
+  maximumaymentContainer.innerHTML = Math.round(paymentInformation.maximumPaymentAmount);
+}
+
+
+$(document).ready(recalculate);
+$("#slider-amount").mouseup(recalculate);
+$("#slider-length").mouseup(recalculate);
+
+
+
