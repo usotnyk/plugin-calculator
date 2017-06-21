e75474a10a744ef54c31cc1f24c5966f729e4fe0
Nazneen Samu Maricar <nazneenmaricar@Nazs-MacBook-Pro.local>
2017-06-21 14:15:03 -0400

    resolved enqueues and css rendering

--- plugins/loan_calculator/includes/enqueue.php
+++ plugins/loan_calculator/includes/enqueue.php
@@ -1,24 +1,21 @@
 
 <?php
-echo plugins_url('loan_calculator');
 
+if(!is_admin()){
+  /* FOLLOWING ENQUEUE(s) ARE FOR THE SLIDER */
 
-wp_enqueue_script( 'calculator-script', plugins_url('loan_calculator/public/js/') . 'test.js');
+  wp_enqueue_style ('jquery-ui-css', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
 
+  wp_enqueue_style ('slider-style', plugins_url('loan_calculator/public/css/') . 'sliderstyle.css');
 
-/* FOLLOWING ENQUEUE(s) ARE FOR THE SLIDER */
-if (!is_admin){
-wp_enqueue_style ('slider-style', plugins_url('loan_calculator/public/css/') . 'sliderstyle.css'); }
+  wp_enqueue_script ('jquery', 'https://code.jquery.com/jquery-1.12.4.js', array('jquery'), 3.3, true);
 
-wp_enqueue_style ('jquery-ui-css', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+  wp_enqueue_script ('jquery-ui-js', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', array('jquery'), 3.3, true);
 
-wp_enqueue_script ('jquery', 'https://code.jquery.com/jquery-1.12.4.js', array('jquery'), 3.3, true); // or null, null, true
+  wp_enqueue_script ('jquery-ui-touch-punch', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js', array('jquery'), 3.3, true);
 
-wp_enqueue_script ('jquery-ui-js', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', array('jquery'), 3.3, true); // or null, null, true
+  wp_enqueue_script ('slider-script', plugins_url('loan_calculator/public/js/') . 'slider.js');
 
-wp_enqueue_script ('jquery-ui-touch-punch', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js', array('jquery'), 3.3, true); // or null, null, true
-
-wp_enqueue_script ('slider-script', plugins_url('loan_calculator/public/js/') . 'slider.js');
-
-wp_enqueue_script ('test-script', plugins_url('loan_calculator/public/js/') . 'test.js');
+  wp_enqueue_script ('test-script', plugins_url('loan_calculator/public/js/') . 'test.js');
+}
 ?>
--- plugins/loan_calculator/public/js/slider.js
+++ plugins/loan_calculator/public/js/slider.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
   $("#slider").slider({
+    range: "min",
     value: 5000,
     min: 5000,
     max: 150000,
@@ -11,12 +12,13 @@ jQuery(document).ready(function($) {
   $("#amount").val("$" + $("#slider").slider("value"));
 
   $("#term-slider").slider({
+    range: "min",
     value: 3,
     min: 3,
     max: 24,
     step: 3,
     slide: function(event, ui) {
-      $("#term").val(ui.value + "term");
+      $("#term").val(ui.value + " months");
     }
   });
   $("#term").val($("#term-slider").slider("value") + " months");
