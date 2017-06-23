8793d798a0af32212584fffa01863482de3efab0
Julia Sotnyk <ulia_sotnik@hotmail.com>
2017-06-23 09:47:04 -0400

    fixed conflict in slider.js after merge

--- plugins/loan_calculator/includes/loan-calculator-public.php
+++ plugins/loan_calculator/includes/loan-calculator-public.php
@@ -18,8 +18,7 @@
       <label for="total-loan-amount"> Your estimated bi-weekely payment </label>
     <!--   <p> $4,028 - $4,473 </p> -->
       <!-- dynam amount from js -->
-      <p> <span id="minimum-payment"></span> </p>
-      <p> <span id="maximum-payment"></span> </p>
+      <p><span id="minimum-payment"></span> - <span id="maximum-payment"></span></p>
 
       <a href="" class="button">Get Your Free Quote </a>
     </div>
@@ -27,7 +26,7 @@
 
   <section class="flex-media dir-col-media width-60-media height-media just-cont-cent-media bg-media">
     <h3> With Lendified you could </h3>
-    <h3 class="text-green"> save up to <span id="amount-saved">$13,015</span> in interest </h3>
+    <h3 class="text-green"> save up to <span id="amount-saved"></span> in interest </h3>
     <img src="bar.png" alt="temporary bar">
     <h6 class="text-center">Estimated Total Interest Cost </h6>
   </section>
--- plugins/loan_calculator/public/js/slider.js
+++ plugins/loan_calculator/public/js/slider.js
@@ -1,38 +1,33 @@
-jQuery(document).ready(function($) {
-<<<<<<< HEAD
-  $("#amount-slider").slider({
-=======
-  for(key in wp_ranges) {
-    wp_ranges[key] = parseInt(wp_ranges[key]);
-  } 
+// jQuery(document).ready(function($) {
+//   for(key in wp_ranges) {
+//     wp_ranges[key] = parseInt(wp_ranges[key]);
+//   }
 
-  $("#slider-amount").slider({
->>>>>>> 5d616db33ebddfc72b6fdf6fff0cc8d7e296c71f
-    range: "min",
-    value: 75000,
-    min: wp_ranges.loanAmountMin,
-    max: wp_ranges.loanAmountMax,
-    step: 1000,
-    slide: function(event, ui) {
-      $("#amount").val("$" + ui.value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-    }
-  });
-<<<<<<< HEAD
-  $("#amount").val("$" + $("#amount-slider").slider("value"));
-=======
-  $("#amount").val("$" + $("#slider-amount").slider("value").toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
->>>>>>> 5d616db33ebddfc72b6fdf6fff0cc8d7e296c71f
+//   $("#slider-amount").slider({ 
+//     range: "min",
+//     value: 75000,
+//     min: wp_ranges.loanAmountMin,
+//     max: wp_ranges.loanAmountMax,
+//     step: 1000,
+//     slide: function(event, ui) {
+//       $("#amount").val("$" + ui.value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+//     }
+//   });
 
-  $("#slider-term").slider({
-    range: "min",
-    value: 3,
-    min: wp_ranges.loanTermMin,
-    max: wp_ranges.loanTermMax,
-    step: 3,
-    slide: function(event, ui) {
-      $("#term").val(ui.value + " months");
-    }
-  });
-  $("#term").val($("#slider-term").slider("value") + " months");
+//   //$("#amount").val("$" + $("#amount-slider").slider("value"));
 
-});
+//   $("#amount").val("$" + $("#slider-amount").slider("value").toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+
+//   $("#slider-term").slider({
+//     range: "min",
+//     value: 3,
+//     min: wp_ranges.loanTermMin,
+//     max: wp_ranges.loanTermMax,
+//     step: 3,
+//     slide: function(event, ui) {
+//       $("#term").val(ui.value + " months");
+//     }
+//   });
+//   $("#term").val($("#slider-term").slider("value") + " months");
+
+// });
