53a1dd51726ab0386e10ee3ba75bb85ccff2b8d1
Jesse Williams <jwilliams8@me.com>
2017-06-27 11:44:53 -0400

    removed enqueue file and placed code within class-loan-calculator, moved loan-calculator-settings & loan-calculator-admin to admin partials folder, moved loan-calculator-public to public partials folder, created functions within settings file to reduce code length for sematic and DRY purposes

--- plugins/loan_calculator/admin/partials/loan-calculator-admin-display.php
+++ plugins/loan_calculator/admin/partials/loan-calculator-admin-display.php
@@ -13,4 +13,31 @@
  */
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <h1><?= esc_html(get_admin_page_title()); ?></h1>
+    <?php settings_errors(); ?>
+    <form action="options.php" method="post">
+        <?php 
+        	settings_fields('loan-calculator-settings-group');
+            echo "<div class='settings-group width-50'>";
+            do_settings_sections('interest_rates');
+            echo "</div>";
+        	echo "<div class='settings-group'>";
+        	do_settings_sections('loan_amounts');
+        	echo "</div>";
+            echo "<div class='settings-group'>";
+        	do_settings_sections('loan_terms');
+            echo "</div>";
+            echo "<div class='settings-group'>";
+            do_settings_sections('pop_ups');
+            echo "</div>";
+            echo "<div class='settings-group'>";
+            do_settings_sections('labels');
+            echo "</div>";
+            echo "<div class='settings-group'>";
+            do_settings_sections('links');
+            echo "</div>";
+        	submit_button('Save Settings');
+        ?>
+    </form>
+</div>
